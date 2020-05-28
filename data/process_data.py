5f0e0c369f5bade2cc9e034ba21b880daad22ae3
Faisal Barazi <f.barazi@gmail.com>
2020-05-27 22:53:20 -0400

    Add clean_data function body.
    clean the data by spliting the categories on `;` and expand the dataframe
    using these values and due proper datatype transformation and clean duplicates.

--- data/process_data.py
+++ data/process_data.py
@@ -16,7 +16,34 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
-    pass
+    """
+    Gets a DataFrame from the load_data output
+    and cleans it furhter.
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # the values are in the form `value-0`, here we get rid of the
+    # last 2 values.
+    category_colnames = row.apply(lambda x: x[:-2])
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    # drop the original categories column from `df`
+    df.drop(columns=['categories'], inplace=True)
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
